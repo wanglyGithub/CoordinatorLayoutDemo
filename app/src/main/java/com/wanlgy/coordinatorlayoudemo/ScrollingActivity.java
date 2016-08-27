c7181f17133f387fc8365a733bf158b2d6e7b2aa
wanglyGithub <wlytctw@163.com>
2016-08-27 19:24:27 +0800

    first commit

---
+++ app/src/main/java/com/wanlgy/coordinatorlayoudemo/ScrollingActivity.java
@@ -0,0 +1,38 @@
+package com.wanlgy.coordinatorlayoudemo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private CollapsingToolbarLayout toolbar_layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        toolbar_layout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolbar_layout.setExpandedTitleColor(Color.WHITE);
+        toolbar_layout.setCollapsedTitleTextColor(Color.WHITE);
+    }
+
+
+}
